7671b6925fba8a788ce557f82acda93fc664c383
kareem337 <74376900+kareem337@users.noreply.github.com>
2022-03-25 17:24:14 +0200

    database

---
+++ database/migrations/2022_03_15_142316_create_matches_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->string('player1');
+            $table->string('player2');
+            $table->foreignId('game_id');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->dateTime('datetime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('game_id');
+            $table->dropColumn('game_id');
+        });
+    }
+}
