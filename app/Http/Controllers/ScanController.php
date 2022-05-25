fae18b46c0cce2c16ab4e4cae558d3e1552e4cc1
Mostafa Khaled <mustafakhaleedd@gmail.com>
2022-05-25 03:09:07 +0200

    comment

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -10,9 +10,11 @@ class ScanController extends Controller
 {
     
     public function match_scan(Request $request){
+        # insert
         Scan::insert(
             ['code' => $request->code, 'game_id' => $request->game, 'device_id' => $request->device, 'time' => $request->time]
         );
+        # delete
         AvailableTimes::where('device_id', '=' ,$request->device, 'AND', 'time', '=', $request->time)->first()->delete();
         return redirect()->back()->with('msg', 'Inserted Successfully');
     }
